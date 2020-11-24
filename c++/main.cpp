ce0a18cc57109172fcf3e3804c4950f6a8f69eed
Andrew <almatinez101@yandex.ru>
2020-11-25 00:06:55 +0700

    math done

---
+++ c++/main.cpp
@@ -0,0 +1,31 @@
+#include <random>
+#include <iostream>
+#include <fstream>
+#include "double_pendulum.hpp"
+
+void pendulum_loop(int steps, std::ofstream data) {
+    
+}
+
+int main(){
+    int steps = 100;
+
+    std::ofstream file;
+    file.open("data.txt");
+
+    dp::state st{{3.14, 1.5}, {0, 0}};
+    dp::system ss{{1, 1}, {2, 3}};
+
+    int k = 0;
+
+    while (k < steps){
+        k++;
+        std::cout << "1st: " << st.theta.first << "; 2nd: " << st.theta.second << std::endl;
+        file << st.theta.first << ";" << st.theta.second << std::endl;
+        st = dp::advance(st, ss, 0.2);
+    }
+
+    file.close();
+
+    return 0;
+}
