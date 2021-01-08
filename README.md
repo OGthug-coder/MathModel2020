01386ed46c2699bc8ebe34c2c679452b830ee8cc
Andrew <almatinez101@yandex.ru>
2021-01-08 16:28:34 +0700

    readme + frontend

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # MathModel2020
+Проект по дисциплине математическое моделирование, 5 семестр
+# Описание проекта
+Реализация модели составного двойного маятника
+
+[![N|Solid](https://sun9-8.userapi.com/impf/hbZVZmWXvDw2vzkR_nR_VzlwQFfgx1XyU8zfrA/KTgKNrsoFDw.jpg?size=722x812&quality=96&proxy=1&sign=1193fe0e01a4ebc8e782b56baef77ac4&type=album)]()
+
+# Параметры системы:
+  - Массы стержней $$m_1, m_2$$
+  - Длины стержней $$l_1, l_2$$
+ 
+# Начальные условия:
+- Начальные углы отклонения $$\theta_1, \theta_2$$
+- Начальные угловые скорости: $$\omega_1, \omega_2$$
+
+# Запуск программы
+
+```console
+cd src/c++
+c++ main.cpp
+./a.out
+```
+После выполнения программы в директории src/c++ будет создан файл file.txt, в котором записано решение задачи. 
+
+Вы можете открыть в браузере визуализацию модели src/frontend/index.html и загрузить file.txt.
+
+[![N|Solid](https://sun9-49.userapi.com/impf/Qfg2VI1MObWJ1uslM-Ia8u-7XzMeI6qVTKsmew/FpNDlGOZonU.jpg?size=1494x1625&quality=96&proxy=1&sign=d46b4d0ff7e9fc856d89243031a45d2b&type=album)]()
--- src/frontend/script.js
+++ src/frontend/script.js
@@ -2,8 +2,8 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 ctx.lineWidth = 10;
 
-let x0 = 700 / 2;
-let y0 = 700 / 2;
+let x0 = 500;
+let y0 = 500;
 
 let l1 = 100;
 let l2 = 100;
@@ -11,14 +11,17 @@ let l2 = 100;
 let N = 0;
 let PHI, THETA;
 
-let timer;
+control = () => {
+    draw(PHI[N], THETA[N]);
+    N++;
+};
 
 prepareData = (text) => {
     let phiValues = [];
     let thetaValues = [];
     let tmp = text.split(/\r?\n/);
 
-    for (let i = 0; i < tmp.length; i++) {
+    for (let i = 0; i < tmp.length; i++){
         let pair = tmp[i].split(";");
         phiValues.push(parseFloat(pair[0]));
         thetaValues.push(parseFloat(pair[1]));
@@ -26,69 +29,24 @@ prepareData = (text) => {
     return [phiValues, thetaValues];
 };
 
-stopModel = () => {
-    clearInterval(timer);
-    N = 0;
-};
+readFile = (input) => {
+    let file = input.files[0];
+    let reader = new FileReader();
+    reader.readAsText(file);
 
-startModel = () => {
-    if (N !== 0) stopModel()
-    fetchData();
-};
-
-fetchData = () => {
-    let body = {
-        steps: document.getElementById("steps").value,
-        l_1: document.getElementById("l_1").value,
-        l_2: document.getElementById("l_2").value,
-        m_1: document.getElementById("m_1").value,
-        m_2: document.getElementById("m_2").value,
-        angle_1: document.getElementById("angle_1").value,
-        angle_2: document.getElementById("angle_2").value,
-        speed_1: document.getElementById("speed_1").value,
-        speed_2: document.getElementById("speed_2").value
+    reader.onload = function() {
+        let data = prepareData(reader.result);
+        let phi = data[0];
+        let theta = data[1];
+        PHI = phi;
+        THETA = theta;
+        setInterval(control, 100);
     };
-
-    for (let [key, value] of Object.entries(body)) {
-        // convert deg to rad
-        if (key.includes("angle")) {
-            value = (parseInt(value) * Math.PI / 180).toFixed(2);
-        }
-        body[key] = parseFloat(value);
-    }
-
-    console.log(body);
-
-    fetch("http://localhost:8085", {method: "POST", body: JSON.stringify(body)})
-        .then(response => response.text())
-        .then(text => {
-            let data = prepareData(text);
-            let phi = data[0];
-            let theta = data[1];
-            PHI = phi;
-            THETA = theta;
-            timer = setInterval(() => {
-                draw(PHI[N], THETA[N]);
-                N++;
-                if (PHI[N] === undefined || THETA[N] === undefined) {
-                    clearInterval(timer);
-                }
-            }, 100);
-        });
 };
-
-// readFile = (input) => {
-//     let file = input.files[0];
-//     let reader = new FileReader();
-//     reader.readAsText(file);
-//
-//     reader.onload = function () {
-//         let data = prepareData(reader.result);
-//     };
-// };
 
 draw = (phi, theta) => {
-    ctx.clearRect(0, 0, 700, 700);
+    console.log(phi, theta)
+    ctx.clearRect(0, 0, 1000, 1000);
 
     ctx.beginPath();
     ctx.arc(x0, y0, 10, 0, 2 * Math.PI);
@@ -106,4 +64,3 @@ draw = (phi, theta) => {
     ctx.lineTo(x0 + l1 * Math.sin(phi) + l2 * Math.sin(theta), y0 + l1 * Math.cos(phi) + l2 * Math.cos(theta));
     ctx.stroke();
 };
-
